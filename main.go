76bcf865d232fec963fdfa41684c4a0af2321e43
njdaniel <nicholasjdaniel@gmail.com>
2022-02-08 13:00:32 -0500

    check token

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -11,19 +10,10 @@ import (
 	"golang.org/x/oauth2"
 )
 
-var (
-	name        = flag.String("name", "", "Name of repo to create in authenticated user's GitHub account.")
-	description = flag.String("description", "", "Description of created repo.")
-	private     = flag.Bool("private", false, "Will created repo be private.")
-)
-
 func main() {
 	token := os.Getenv("GITHUB_TOKEN")
 	if token == "" {
 		log.Fatal("Unauthorized: No token present")
-	}
-	if *name == "" {
-		log.Fatal("No name: New repos must be given a name")
 	}
 	ctx := context.Background()
 	ts := oauth2.StaticTokenSource(
