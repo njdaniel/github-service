2fbddb0aaddf95016f5ff723e75028e602b94f95
njdaniel <nicholasjdaniel@gmail.com>
2022-02-09 09:53:15 -0500

    create from template

---
+++ services/create-repo-from-template/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/google/go-github/v42/github"
+	"golang.org/x/oauth2"
+)
+
+var (
+	name        = flag.String("name", "", "Name of repo to create in authenticated user's GitHub account.")
+	description = flag.String("description", "", "Description of created repo.")
+	private     = flag.Bool("private", false, "Will created repo be private.")
+)
+
+func main() {
+	flag.Parse()
+	token := os.Getenv("GITHUB_TOKEN")
+	if token == "" {
+		log.Fatal("Unauthorized: No token present")
+	}
+	if *name == "" {
+		log.Fatal("No name: New repos must be given a name")
+	}
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})
+	tc := oauth2.NewClient(ctx, ts)
+	client := github.NewClient(tc)
+
+	//https://github.com/njdaniel/api-template
+	//get template repo
+	template, _, err := client.Repositories.Get(ctx, "njdaniel", "api-template")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%T\n", template)
+
+	r := &github.Repository{Name: name, Private: private, Description: description, TemplateRepository: template}
+	repo, _, err := client.Repositories.Create(ctx, "", r)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Successfully created new repo: %v\n", repo.GetName())
+}
